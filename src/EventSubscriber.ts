d6050eeee3c624b84bf964fa38a235066efba819
Florin Munteanu <florinmunteanu96@gmail.com>
2017-04-29 16:20:23 +0300

    Add event subscriber
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

---
+++ src/EventSubscriber.ts
@@ -0,0 +1,22 @@
+enum EventType{
+    RightClick,LeftClick,UP,DOWN,LEFT,RIGHT
+}
+
+interface IEvent{
+    Type : EventType;
+    EventAction : IEventAction;
+}
+
+interface IEventAction{
+    ():void;
+}
+
+class InputEvent implements IEvent{
+    Type:EventType;
+    EventAction:IEventAction;
+
+    constructor(name:EventType,eventAction:IEventAction){
+        this.Type = name;
+        this.EventAction = eventAction;
+    }
+}
