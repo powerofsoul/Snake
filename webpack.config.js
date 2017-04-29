11a70c8d05c3aa53bc46abd9815e3cc1a7cfed9f
Florin Munteanu <florinmunteanu96@gmail.com>
2017-04-29 08:56:47 +0300

    Clear webpack config
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,5 @@ module.exports = {
    filename: './out/game.js',
    path: __dirname
  },
- module: {
-   rules: [
-     {
-       enforce: 'pre',
-       test: /\.js$/,
-       loader: "source-map-loader"
-     },
-     {
-       enforce: 'pre',
-       test: /\.tsx?$/,
-       use: "source-map-loader"
-     }
-   ]
- },
- resolve: {
-   extensions: [".tsx", ".ts", ".js"]
- },
  devtool: 'inline-source-map',
 };
