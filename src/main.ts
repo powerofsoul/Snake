1c0ef9d930e83a7f85e9637fefaacf5b687c6716
Florin Munteanu <florinmunteanu96@gmail.com>
2017-04-29 08:17:11 +0300

    Add three dependencies
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,48 @@
-class Main{
-    public WriteToLog() {
-        console.log("test");
+import  THREE = require('three');
+
+var scene: THREE.Scene;
+var camera: THREE.PerspectiveCamera;
+var renderer = new THREE.WebGLRenderer();
+var geometry;
+var material;
+var mesh = new THREE.Mesh();
+
+export class Game {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+
+        scene = new THREE.Scene();
+
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        camera.position.z = 1000;
+
+        geometry = new THREE.BoxGeometry(200, 200, 200);
+        material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+
+        mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        console.log(renderer);
+        document.body.appendChild(renderer.domElement);
+    }
+
+    animate() {
+
+        requestAnimationFrame(this.animate);
+
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.02;
+
+        renderer.render(scene, camera);
+
     }
 }
-
-var m = new Main();
-m.WriteToLog();
+function StartGame(){
+    var m = new Game();
+}
+window.onload = StartGame;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+ entry: './compiled/main.js',
+ output: {
+   filename: './out/game.js',
+   path: __dirname
+ },
+ module: {
+   rules: [
+     {
+       enforce: 'pre',
+       test: /\.js$/,
+       loader: "source-map-loader"
+     },
+     {
+       enforce: 'pre',
+       test: /\.tsx?$/,
+       use: "source-map-loader"
+     }
+   ]
+ },
+ resolve: {
+   extensions: [".tsx", ".ts", ".js"]
+ },
+ devtool: 'inline-source-map',
+};
