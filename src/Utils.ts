527028f23575edca8f0ce8d2ef915cc24d951b45
Florin Munteanu <florinmunteanu96@gmail.com>
2017-04-29 09:31:43 +0300

    Add utils class
    Signed-off-by: Florin Munteanu <florinmunteanu96@gmail.com>

---
+++ src/Utils.ts
@@ -0,0 +1,30 @@
+export namespace Utils {
+    class List<T>{
+        private items: T[];
+        private size :number;
+
+        public constructor(){
+            this.clear();
+        }
+        
+        public clear(){
+            this.items = [];
+            this.size = 0;
+        }
+
+        public add(item:T){
+            this.items.push(item);
+        }
+
+        public removeAt(index:number){
+            this.items.splice(index,1);
+        }
+
+        public removeItem(item:T){
+            var index = this.items.findIndex(i=> i == item);
+            if(index>=0){
+                this.removeAt(index);
+            }
+        }
+    }
+}
