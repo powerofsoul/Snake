bba2e5d2f7dab96dabd152d82154ec11d027ee3d
Florin Munteanu <florinmunteanu96@gmail.com>
2017-05-06 19:07:31 +0300

    Revert "Add collision"
    This reverts commit 4745418a64851e47e3e266aa900cb36c56fac48e.

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,21 @@ import THREE = require('three');
 import { GameWindow } from "./GameWindow";
 import { Snake } from "./Snake";
 import { InputEvent, EventType } from "./EventSubscriber"
-import { BodyGeometry,Direction } from "./SpaceElement";
+
 export class Game {
     static gameSpeed: number = 10;
-    static gameWindow : GameWindow;
 
-    static init():any {
-        Game.gameWindow = new GameWindow();
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        var gameWindow = new GameWindow();
         var snake = new Snake(new THREE.Vector3(0, 0, 0), "white");
-        Game.gameWindow.AddToWindow(snake);
+        gameWindow.AddToWindow(snake);
     }
 }
-
-window.onload = Game.init;
+function StartGame() {
+    var m = new Game();
+}
+window.onload = StartGame;
